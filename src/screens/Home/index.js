61f1399260cfb9c23acf4dbc093a839a16b83ec6
froliveira94 <fr.oliveira94@gmail.com>
2019-04-25 11:45:49 -0300

    add: basic structure

---
+++ src/screens/Home/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+import "./style.scss";
+
+class Home extends Component {
+  render() {
+    return <p className="home">Home</p>;
+  }
+}
+
+export default Home;
